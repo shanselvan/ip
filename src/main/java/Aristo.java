6aae05ad1066fa2b473b2374c640b9dd1728d780
Shan Selvan <skillfulshan@gmail.com>
2026-01-19 14:32:04 +0800

    Add exit message

--- src/main/java/Aristo.java
+++ src/main/java/Aristo.java
@@ -1,12 +1,21 @@
 public class Aristo {
     public static void main(String[] args) {
         greet();
+        exit();
     }
 
     public static void greet() {
         System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * *");
         System.out.println("Hello, human!");
         System.out.println("Aristo here and ready to assist.");
+        System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * *");
+    }
+
+    public static void exit() {
+        System.out.println();
+        System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * *");
+        System.out.println("Goodbye!");
+        System.out.println("Aristo awaits your return...");
         System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * *");
     }
 }
