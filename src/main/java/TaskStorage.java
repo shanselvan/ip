70c5e456e873eed1c72675f3ae6b4a5d80cf2369
Shan Selvan <skillfulshan@gmail.com>
2026-01-25 16:17:29 +0800

    Add method to load tasks from file

--- src/main/java/TaskStorage.java
+++ src/main/java/TaskStorage.java
@@ -26,4 +26,51 @@ public class TaskStorage {
             System.out.println("Could not initialise data file! " + e.getMessage());
         }
     }
+
+    public ArrayList<Task> loadTasks() {
+        ArrayList<Task> loadedTasks = new ArrayList<>();
+        try (Scanner scanner = new Scanner(filePath)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(" \\| ");
+                String taskType = parts[0];
+                boolean isDone = parts[1].equals("1");
+                String taskDescription = parts[2];
+
+                Task task;
+
+                switch (taskType) {
+
+                case "T":
+                    task = new Todo(taskDescription);
+                    break;
+
+                case "D":
+                    String by = parts[3];
+                    task = new Deadline(taskDescription, by);
+                    break;
+
+                case "E":
+                    String from = parts[3];
+                    String to = parts[4];
+                    task = new Event(taskDescription, from, to);
+                    break;
+
+                default:
+                    continue;
+
+                }
+
+                if (isDone) {
+                    task.markAsDone();
+                }
+
+                loadedTasks.add(task);
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while loading tasks! " + e.getMessage());
+        }
+
+        return loadedTasks;
+    }
 }
