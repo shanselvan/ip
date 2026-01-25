815a2019f12c8c0f72a5eba5fd42395f3568dae3
Shan Selvan <skillfulshan@gmail.com>
2026-01-25 17:57:44 +0800

    Implement toFileString method for each task

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -12,4 +12,10 @@ public class Event extends Task {
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
+
+    @Override
+    public String toFileString() {
+        int isDone = this.isDone ? 1 : 0;
+        return "E | " + isDone + " | " + this.description + " | " + from + " | " + to;
+    }
 }
