0db14a2bdb5e5949bf230137e112d181d0a6dd4e
Shan Selvan <skillfulshan@gmail.com>
2026-01-25 22:00:19 +0800

    Use LocalDate for deadline due date

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,24 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate by;
+    private static final DateTimeFormatter INPUT_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter OUTPUT_FORMATTER = DateTimeFormatter.ofPattern("MMM dd yyyy");
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDate.parse(by, INPUT_FORMATTER);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(OUTPUT_FORMATTER) + ")";
     }
 
     @Override
     public String toFileString() {
         int isDone = this.isDone ? 1 : 0;
-        return "D | " + isDone + " | " + this.description + " | " + this.by;
+        return "D | " + isDone + " | " + this.description + " | " + by.format(INPUT_FORMATTER);
     }
 }
