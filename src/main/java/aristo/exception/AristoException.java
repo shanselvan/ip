e8615da6d9c38274226136ed5250bce262032263
Shan Selvan <skillfulshan@gmail.com>
2026-01-27 23:00:37 +0800

    Add JavaDoc comments to AristoException class

--- src/main/java/aristo/exception/AristoException.java
+++ src/main/java/aristo/exception/AristoException.java
@@ -1,5 +1,11 @@
 package aristo.exception;
 
+/**
+ * Represents exceptions specific to the Aristo chatbot.
+ * <p>
+ * This exception is thrown when the user input is invalid or when
+ * a task operation cannot be performed.
+ */
 public class AristoException extends Exception {
 
     public AristoException(String message) {
