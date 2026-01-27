761c87b1265d2f4458125838564ef06767109004
Shan Selvan <skillfulshan@gmail.com>
2026-01-27 22:41:48 +0800

    Add JavaDoc comments to TaskList class

--- src/main/java/aristo/task/TaskList.java
+++ src/main/java/aristo/task/TaskList.java
@@ -4,6 +4,11 @@ import aristo.exception.AristoException;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a list of tasks in the Aristo chatbot.
+ * <p>
+ * This class provides methods to add, remove, retrieve, and query tasks.
+ */
 public class TaskList {
     private final ArrayList<Task> tasks;
 
@@ -11,14 +16,27 @@ public class TaskList {
         this.tasks = new ArrayList<>(tasks);
     }
 
+    /**
+     * Returns the number of tasks in this task list.
+     */
     public int size() {
         return tasks.size();
     }
 
+    /**
+     * Adds a given task to this task list.
+     */
     public void addTask(Task task) {
         this.tasks.add(task);
     }
 
+    /**
+     * Removes and returns the task at the given index.
+     *
+     * @param index 1-based index of the task to remove.
+     * @return The task that was removed.
+     * @throws AristoException If the given index is invalid.
+     */
     public Task removeTask(int index) throws AristoException {
         if (index > tasks.size() || index < 1) {
             throw new AristoException("Invalid task number! Please retry with a valid task number.\n");
@@ -26,6 +44,13 @@ public class TaskList {
         return tasks.remove(index - 1);
     }
 
+    /**
+     * Returns the task at the given index.
+     *
+     * @param index 1-based index of the task to remove.
+     * @return The task at the given index.
+     * @throws AristoException If the given index is invalid.
+     */
     public Task getTask(int index) throws AristoException {
         if (index > tasks.size() || index < 1) {
             throw new AristoException("Invalid task number! Please retry with a valid task number.\n");
@@ -33,10 +58,16 @@ public class TaskList {
         return tasks.get(index - 1);
     }
 
+    /**
+     * Returns a copy of this task list.
+    */
     public ArrayList<Task> asList() {
         return new ArrayList<>(tasks);
     }
 
+    /**
+     * Returns whether this task list is empty.
+     */
     public boolean isEmpty() {
         return tasks.isEmpty();
     }
