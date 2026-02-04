d82d347169521739a8f205ff6f98c64b650a205b
Shan Selvan <skillfulshan@gmail.com>
2026-02-05 01:14:09 +0800

    Add files to support GUI

---
+++ src/main/java/aristo/Main.java
@@ -0,0 +1,34 @@
+package aristo;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Duke using FXML.
+ */
+public class Main extends Application {
+
+    private Aristo aristo = new Aristo();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
+
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<aristo.MainWindow>getController().setAristo(aristo);  // inject the Aristo instance
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
