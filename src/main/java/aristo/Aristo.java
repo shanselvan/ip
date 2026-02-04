d6ac91b6d449e9051490e9cb1b0a27be907924c0
Shan Selvan <skillfulshan@gmail.com>
2026-02-05 01:00:12 +0800

    Add getResponse method to Aristo class

--- src/main/java/aristo/Aristo.java
+++ src/main/java/aristo/Aristo.java
@@ -18,6 +18,7 @@ import aristo.ui.Ui;
 public class Aristo {
     private static TaskList taskList;
     private static final Ui ui = new Ui();
+    private static final TaskStorage storage = new TaskStorage("./data/aristo.txt");
 
     private static final String COMMAND_LIST = "list";
     private static final String COMMAND_MARK = "mark";
@@ -27,6 +28,7 @@ public class Aristo {
     private static final String COMMAND_DEADLINE = "deadline";
     private static final String COMMAND_EVENT = "event";
     private static final String COMMAND_BYE = "bye";
+    private static final String COMMAND_FIND = "find";
 
 
     /**
@@ -37,87 +39,81 @@ public class Aristo {
      * @param args command-line arguments (not used)
      */
     public static void main(String[] args) {
-        TaskStorage storage = new TaskStorage("./data/aristo.txt");
-        taskList = new TaskList(storage.loadTasks());
-
+        Aristo aristo = new Aristo();
         ui.greet();
         String userInput = ui.fetchNextCommand();
 
         while (!userInput.equals(COMMAND_BYE)) {
-            try {
-                String[] parsed = Parser.parseCommand(userInput);
-                String command = parsed[0];
-                String taskIndexString = parsed[1];
-
-                switch (command) {
-                case COMMAND_LIST:
-                    ui.printTaskList(taskList);
-                    break;
-
-                case COMMAND_MARK:
-                    int taskIndexInteger = Parser.parseTaskIndex(taskIndexString);
-                    Aristo.handleMarkTask(taskIndexInteger);
-                    break;
-
-                case COMMAND_UNMARK:
-                    if (taskIndexString.isBlank()) {
-                        throw new AristoException("Please specify a task number to unmark!\n");
-                    }
-
-                    int taskIndex = Integer.parseInt(taskIndexString);
-                    Aristo.handleUnmarkTask(taskIndex);
-                    break;
-
-                case COMMAND_DELETE:
-                    if (taskIndexString.isBlank()) {
-                        throw new AristoException("Please specify a task number to delete!\n");
-                    }
-
-                    int taskIndexInt = Integer.parseInt(taskIndexString);
-                    Aristo.handleDeleteTask(taskIndexInt);
-                    break;
-
-                case COMMAND_TODO:
-                    Aristo.handleTodo(taskIndexString);
-                    break;
-
-                case COMMAND_DEADLINE:
-                    Aristo.handleDeadline(taskIndexString);
-                    break;
-
-                case COMMAND_EVENT:
-                    Aristo.handleEvent(taskIndexString);
-                    break;
-
-                case "find":
-                    if (taskIndexString.isBlank()) {
-                        throw new AristoException("Please provide a keyword to search for!\n");
-                    }
-
-                    TaskList matchingTasks = taskList.find(taskIndexString);
-                    ui.printMatchingTasks(matchingTasks);
-                    break;
-
-                default:
-                    throw new AristoException("Ehm, never heard of that command before!\n");
-                }
-                storage.saveTasks(taskList);
-
-            } catch (AristoException e) {
-                ui.showError(e.getMessage());
-            }
-
+            aristo.getResponse(userInput);
             userInput = ui.fetchNextCommand();
         }
 
         ui.exit();
     }
 
+    public String getResponse(String input) {
+        taskList = new TaskList(storage.loadTasks());
+
+        try {
+            String[] parsed = Parser.parseCommand(input);
+            String command = parsed[0];
+            String taskIndexString = parsed[1];
+
+            switch (command) {
+            case COMMAND_LIST:
+                return ui.printTaskList(taskList);
+
+            case COMMAND_MARK:
+                int taskIndexInteger = Parser.parseTaskIndex(taskIndexString);
+                return handleMarkTask(taskIndexInteger);
+
+            case COMMAND_UNMARK:
+                if (taskIndexString.isBlank()) {
+                    throw new AristoException("Please specify a task number to unmark!\n");
+                }
+
+                int taskIndex = Integer.parseInt(taskIndexString);
+                return handleUnmarkTask(taskIndex);
+
+            case COMMAND_DELETE:
+                if (taskIndexString.isBlank()) {
+                    throw new AristoException("Please specify a task number to delete!\n");
+                }
+
+                int taskIndexInt = Integer.parseInt(taskIndexString);
+                return handleDeleteTask(taskIndexInt);
+
+            case COMMAND_TODO:
+                return handleTodo(taskIndexString);
+
+            case COMMAND_DEADLINE:
+                return handleDeadline(taskIndexString);
+
+            case COMMAND_EVENT:
+                return handleEvent(taskIndexString);
+
+            case COMMAND_FIND:
+                if (taskIndexString.isBlank()) {
+                    throw new AristoException("Please provide a keyword to search for!\n");
+                }
+
+                TaskList matchingTasks = taskList.find(taskIndexString);
+                return ui.printMatchingTasks(matchingTasks);
+
+            default:
+                throw new AristoException("Ehm, never heard of that command before!\n");
+            }
+
+        } catch (AristoException e) {
+            return ui.showError(e.getMessage());
+        }
+    }
+
     /**
      * Displays the number of tasks currently in the task list.
      */
-    public static void printNumberOfTasks() {
-        ui.printNumberOfTasks(taskList);
+    public String printNumberOfTasks() {
+        return ui.printNumberOfTasks(taskList);
     }
 
 
@@ -125,9 +121,10 @@ public class Aristo {
      * Marks the specified task as done.
      *
      * @param taskIndexInteger 1-based index of the task to mark.
+     * @return The message string indicating if the task has been marked.
      * @throws AristoException If the task is already marked as done or index is invalid.
      */
-    public static void handleMarkTask(int taskIndexInteger) throws AristoException {
+    public String handleMarkTask(int taskIndexInteger) throws AristoException {
         Task task = taskList.getTask(taskIndexInteger);
 
         if (task.isDone()) {
@@ -135,16 +132,18 @@ public class Aristo {
         }
 
         task.markAsDone();
-        ui.showTaskMarked(task);
+        storage.saveTasks(taskList);
+        return ui.showTaskMarked(task);
     }
 
     /**
      * Marks the specified task as not done.
      *
      * @param taskIndexInteger 1-based index of the task to unmark.
+     * @return The message string indicating if the task has been unmarked.
      * @throws AristoException If the task is already not done or index is invalid.
      */
-    public static void handleUnmarkTask(int taskIndexInteger) throws AristoException {
+    public String handleUnmarkTask(int taskIndexInteger) throws AristoException {
         Task task = taskList.getTask(taskIndexInteger);
 
         if (!task.isDone()) {
@@ -152,53 +151,57 @@ public class Aristo {
         }
 
         task.markAsNotDone();
-        ui.showTaskUnmarked(task);
+        storage.saveTasks(taskList);
+        return ui.showTaskUnmarked(task);
     }
 
     /**
      * Deletes the specified task from the task list.
      *
      * @param taskIndexInteger 1-based index of the task to delete.
+     * @return The message string indicating if the task has been deleted.
      * @throws AristoException If the task index is invalid.
      */
-    public static void handleDeleteTask(int taskIndexInteger) throws AristoException {
+    public String handleDeleteTask(int taskIndexInteger) throws AristoException {
         Task task = taskList.removeTask(taskIndexInteger);
-        ui.showTaskDeleted(task);
-        Aristo.printNumberOfTasks();
+        storage.saveTasks(taskList);
+        return ui.showTaskDeleted(task) + this.printNumberOfTasks();
     }
 
     /**
      * Adds a new todo task to the task list.
      *
      * @param description Description of the todo task.
+     * @return The message string indicating if the task has been added.
      * @throws AristoException If the description is blank.
      */
-    public static void handleTodo(String description) throws AristoException {
+    public String handleTodo(String description) throws AristoException {
         if (description.isBlank()) {
             throw new AristoException("Task description is empty, please retry with a valid task description.\n");
         }
 
         Todo todoTask = new Todo(description);
         taskList.addTask(todoTask);
-        ui.showTodoTaskAdded(todoTask);
-        Aristo.printNumberOfTasks();
+        storage.saveTasks(taskList);
+        return ui.showTodoTaskAdded(todoTask) + this.printNumberOfTasks();
     }
 
     /**
      * Adds a new deadline task to the task list.
      *
      * @param taskDetails Description and deadline (format: "DESCRIPTION /by DEADLINE").
+     * @return The message string indicating if the task has been added.
      * @throws AristoException If input is blank or deadline format is invalid.
      */
-    public static void handleDeadline(String taskDetails) throws AristoException {
+    public String handleDeadline(String taskDetails) throws AristoException {
         if (taskDetails.isBlank()) {
             throw new AristoException("Please provide a task description and its deadline! e.g XXX /by YYY\n");
         }
 
         Deadline deadlineTask = getDeadline(taskDetails);
         taskList.addTask(deadlineTask);
-        ui.showDeadlineTaskAdded(deadlineTask);
-        Aristo.printNumberOfTasks();
+        storage.saveTasks(taskList);
+        return ui.showDeadlineTaskAdded(deadlineTask) + this.printNumberOfTasks();
     }
 
     /**
@@ -225,9 +228,10 @@ public class Aristo {
      * Adds a new <code>Event</code> task to the task list.
      *
      * @param taskDetails Description, start and end times (format: "DESCRIPTION /from START /to END").
+     * @return The message string indicating if the event has been added.
      * @throws AristoException If input is missing description or times, or has invalid date format.
      */
-    public static void handleEvent(String taskDetails) throws AristoException {
+    public String handleEvent(String taskDetails) throws AristoException {
         String[] taskComponents = Parser.parseEvent(taskDetails);
         String description = taskComponents[0];
         String from = taskComponents[1];
@@ -242,8 +246,8 @@ public class Aristo {
         try {
             Event eventTask = new Event(description, from, to);
             taskList.addTask(eventTask);
-            ui.showEventTaskAdded(eventTask);
-            Aristo.printNumberOfTasks();
+            storage.saveTasks(taskList);
+            return ui.showEventTaskAdded(eventTask) + this.printNumberOfTasks();
         } catch (DateTimeParseException e) {
             throw new AristoException("Please use yyyy-MM-dd format for dates!\n");
         }
